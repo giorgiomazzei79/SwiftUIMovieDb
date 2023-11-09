cecb8f8f66fa3ac803ed7035caf0e5a6bde38a05
Giorgio Mazzei <40761205+giorgiomazzei79@users.noreply.github.com>
2023-11-09 12:18:10 +0100

    Initial Commit

---
+++ SwiftUIMovieDb/SwiftUIMovieDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieDbApp.swift
+//  SwiftUIMovieDb
+//
+//  Created by Giorgio Mazzei on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
