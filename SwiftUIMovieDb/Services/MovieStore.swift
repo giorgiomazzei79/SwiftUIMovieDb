4a2f8ad56ce4352f0e149c38d12bd363a3b62955
Giorgio Mazzei <giorgio.mazzei@icloud.com>
2023-12-08 17:31:50 +0100

    feat: add error handling movie store

--- SwiftUIMovieDb/Services/MovieStore.swift
+++ SwiftUIMovieDb/Services/MovieStore.swift
@@ -33,7 +33,51 @@ class MovieStore: MovieService {
             completion(.failure(.invalidEndpoint))
             return
         }
+        
+        var queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
+        if let params = params {
+            queryItems.append(contentsOf: params.map{ URLQueryItem(name: $0.key, value: $0.value)})
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        guard let finalURL = urlComponents.url else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        urlSession.dataTask(with: finalURL) { [weak self] (data, response, error) in
+            guard let self = self else {return}
+            if error != nil {
+                self.executeCompletionHandlerInMainThread(with: .failure(.apiError), completion: completion)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                self.executeCompletionHandlerInMainThread(with: .failure(.invalidResponse), completion: completion)
+                return
+            }
+            
+            guard let data = data else {
+                self.executeCompletionHandlerInMainThread(with: .failure(.noData), completion: completion)
+                return
+            }
+            
+            do {
+                let decodedResponse = try self.jsonDecoder.decode(D.self, from: data)
+                self.executeCompletionHandlerInMainThread(with: .success(decodedResponse), completion: completion)
+            } catch {
+                self.executeCompletionHandlerInMainThread(with: .failure(.serializationError), completion: completion)
+            }
+        }.resume()
+    }
+    
+    private func executeCompletionHandlerInMainThread<D: Decodable>(with result: Result<D, MovieError>, completion: @escaping (Result<D, MovieError>) -> ()){
+        DispatchQueue.main.async {
+            completion(result)
+        }
     }
     
     
 }
+
