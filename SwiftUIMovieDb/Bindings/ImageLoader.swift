3281d0dd477058d506748a50189c46ce21932e41
Giorgio Mazzei <giorgio.mazzei@icloud.com>
2024-01-20 01:29:18 +0100

    feat: create imageloader - wip

---
+++ SwiftUIMovieDb/Bindings/ImageLoader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageLoader.swift
+//  SwiftUIMovieDb
+//
+//  Created by Giorgio Mazzei on 20/01/24.
+//
+
+import Foundation
---
+++ SwiftUIMovieDb/Views/MovieBackdropCard.swift
@@ -0,0 +1,18 @@
+//
+//  MovieBackdropCard.swift
+//  SwiftUIMovieDb
+//
+//  Created by Giorgio Mazzei on 20/01/24.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieBackdropCard()
+}
