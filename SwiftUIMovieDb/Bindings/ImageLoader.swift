6cdeb43288f4a1e757508166ab80e3b66f595758
Giorgio Mazzei <giorgio.mazzei@icloud.com>
2024-01-20 01:31:20 +0100

    feat: create imageloader - wip

--- SwiftUIMovieDb/Bindings/ImageLoader.swift
+++ SwiftUIMovieDb/Bindings/ImageLoader.swift
@@ -5,4 +5,23 @@
 //  Created by Giorgio Mazzei on 20/01/24.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+private let _imageCache = NSCache<AnyObject, AnyObject>()
+
+class ImageLoader: ObservableObject {
+    
+    @Published var image: UIImage?
+    @Published var isLoading = false
+    
+    var imageCache = _imageCache
+    
+    func loadImage (with url: URL) {
+        let urlString = url.absoluteString
+        if let ImageFromCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = ImageFromCache
+            return
+        }
+    }
+}
